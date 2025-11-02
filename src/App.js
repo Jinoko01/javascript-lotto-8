b50b4fa2dc4c2d9cf3f25c997f26ab1f62564b9f
dswvgw6412 <dswvgw6412@naver.com>
2025-11-02 17:15:43 +0900

    refactor(lottoController): Add LottoController
    Separated client and event process logic

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
 import { Console } from '@woowacourse/mission-utils';
-import { LOTTO } from './data/constants.js';
 import LottoFactory from './model/LottoFactory.js';
 import ProfitCalculator from './service/ProfitCalculator.js';
 import RankDeterminer from './service/RankDeterminer.js';
 import WinningChecker from './service/WinningChecker.js';
 import InputView from './view/InputView.js';
 import OutputView from './view/OutputView.js';
+import LottoController from './controller/LottoController.js';
 
 class App {
+  constructor() {
+    this.inputView = new InputView();
+    this.outputView = new OutputView();
+    this.lottoFactory = new LottoFactory();
+    this.winningChecker = new WinningChecker();
+    this.rankDeterminer = new RankDeterminer();
+    this.profitCalculator = new ProfitCalculator();
+    this.lottoController = new LottoController(
+      this.inputView,
+      this.outputView,
+      this.lottoFactory,
+      this.winningChecker,
+      this.rankDeterminer,
+      this.profitCalculator
+    );
+  }
+
   async run() {
     try {
-      const inputView = new InputView();
-      const outputView = new OutputView();
-      const purchaseAmount = await inputView.inputPurChaseAmount();
-
-      const lottoCount = purchaseAmount / LOTTO.AMOUNT;
-      const lottoFactory = new LottoFactory();
-      const lottoList = lottoFactory.createLottos(lottoCount);
-      outputView.printLottos(lottoList);
-
-      const winningNumbers = await inputView.inputWinningNumbers();
-      const bonusNumber = await inputView.inputBonusNumber(winningNumbers);
-
-      const winningChecker = new WinningChecker();
-      const winningResult = winningChecker.checkWinning(lottoList, winningNumbers, bonusNumber);
-
-      const rankDeterminer = new RankDeterminer();
-      const rankInformation = rankDeterminer.determineRank(winningResult);
-      outputView.printResult(rankInformation);
-
-      const profitCalculator = new ProfitCalculator();
-      const profitRate = profitCalculator.calculateProfitRate(rankInformation, purchaseAmount);
-      outputView.printProfit(profitRate);
+      await this.lottoController.run();
     } catch (error) {
       Console.print(error.message);
     }
