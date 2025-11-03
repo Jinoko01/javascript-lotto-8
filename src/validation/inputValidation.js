7387ba13ce242df24388f7b88aee259951647398
dswvgw6412 <dswvgw6412@naver.com>
2025-11-03 20:59:26 +0900

    fix(input): Add input feature
    if throw input error, retry input

--- src/validation/inputValidation.js
+++ src/validation/inputValidation.js
@@ -1,48 +1,49 @@
 import { LOTTO } from '../data/constants.js';
 import { ERROR_MESSAGE } from '../data/messages.js';
 import { REGEX } from '../data/regex.js';
+import LotteryError from '../error/LotteryError.js';
 
 export default class InputValidation {
-  validatePurchaseAmount(purchaseAmount) {
+  validatePurchaseAmount(purchaseAmount, retryInput) {
     if (!REGEX.ONLY_NUMBER.test(purchaseAmount)) {
-      throw new Error(ERROR_MESSAGE.INVALID_PURCHASE_AMOUNT);
+      throw new LotteryError(ERROR_MESSAGE.INVALID_PURCHASE_AMOUNT, retryInput);
     }
 
     const isDivisible = purchaseAmount % LOTTO.AMOUNT === 0;
     if (!isDivisible) {
-      throw new Error(ERROR_MESSAGE.INVALID_PURCHASE_AMOUNT);
+      throw new LotteryError(ERROR_MESSAGE.INVALID_PURCHASE_AMOUNT, retryInput);
     }
   }
 
-  validateWinningNumbersInputString(winningNumbersString) {
+  validateWinningNumbersInputString(winningNumbersString, retryInput) {
     if (!REGEX.ONLY_NUMBER_AND_COMMA.test(winningNumbersString)) {
-      throw new Error(ERROR_MESSAGE.INVALID_WINNING_NUMBERS);
+      throw new LotteryError(ERROR_MESSAGE.INVALID_WINNING_NUMBERS, retryInput);
     }
   }
 
-  validateWinningNumbersDuplicated(winningNumbersArray) {
+  validateWinningNumbersDuplicated(winningNumbersArray, retryInput) {
     if (new Set(winningNumbersArray).size !== LOTTO.COUNT) {
-      throw new Error(ERROR_MESSAGE.INVALID_WINNING_NUMBERS);
+      throw new LotteryError(ERROR_MESSAGE.INVALID_WINNING_NUMBERS, retryInput);
     }
   }
 
-  validateBonusNumberInputString(bonusNumberString) {
+  validateBonusNumberInputString(bonusNumberString, retryInput) {
     if (!REGEX.ONLY_NUMBER.test(bonusNumberString)) {
-      throw new Error(ERROR_MESSAGE.INVALID_BONUS_NUMBER);
+      throw new LotteryError(ERROR_MESSAGE.INVALID_BONUS_NUMBER, retryInput);
     }
   }
 
-  validateBonusNumberRange(bonusNumber, winningNumbers) {
+  validateBonusNumberRange(bonusNumber, winningNumbers, retryInput) {
     const isGraterThanMinNumber = bonusNumber >= LOTTO.MIN_NUMBER;
     const isLessThanMaxNumber = bonusNumber <= LOTTO.MAX_NUMBER;
     const invalidNumberRange = !isGraterThanMinNumber || !isLessThanMaxNumber;
 
     if (invalidNumberRange) {
-      throw new Error(ERROR_MESSAGE.INVALID_BONUS_NUMBER);
+      throw new LotteryError(ERROR_MESSAGE.INVALID_BONUS_NUMBER, retryInput);
     }
 
     if (winningNumbers.includes(bonusNumber)) {
-      throw new Error(ERROR_MESSAGE.INVALID_BONUS_NUMBER);
+      throw new LotteryError(ERROR_MESSAGE.INVALID_BONUS_NUMBER, retryInput);
     }
   }
 }
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,37 +1,46 @@
 import { Console } from '@woowacourse/mission-utils';
 import { SYSTEM_MESSAGE } from '../data/messages.js';
-import InputValidation from '../validation/inputValidation.js';
 
 export default class InputView {
   constructor(validator) {
     this.validation = validator;
   }
+
   async inputPurChaseAmount() {
-    const purchaseAmount = await this.#readLineAsync(SYSTEM_MESSAGE.INPUT_PURCHASE_AMOUNT);
-
-    this.validation.validatePurchaseAmount(purchaseAmount);
-
-    return Number(purchaseAmount);
+    try {
+      const purchaseAmount = await this.#readLineAsync(SYSTEM_MESSAGE.INPUT_PURCHASE_AMOUNT);
+      this.validation.validatePurchaseAmount(purchaseAmount, this.inputPurChaseAmount);
+      return Number(purchaseAmount);
+    } catch (error) {
+      Console.print(error.message);
+      return await this.inputPurChaseAmount();
+    }
   }
 
   async inputWinningNumbers() {
-    const winningNumbers = await this.#readLineAsync(SYSTEM_MESSAGE.INPUT_WINNING_NUMBERS);
-    this.validation.validateWinningNumbersInputString(winningNumbers);
-
-    const numbers = winningNumbers.split(',').map((number) => Number(number));
-    this.validation.validateWinningNumbersDuplicated(numbers);
-
-    return numbers;
+    try {
+      const winningNumbers = await this.#readLineAsync(SYSTEM_MESSAGE.INPUT_WINNING_NUMBERS);
+      this.validation.validateWinningNumbersInputString(winningNumbers, this.inputWinningNumbers);
+      const numbers = winningNumbers.split(',').map((number) => Number(number));
+      this.validation.validateWinningNumbersDuplicated(numbers, this.inputWinningNumbers);
+      return numbers;
+    } catch (error) {
+      Console.print(error.message);
+      return await this.inputWinningNumbers();
+    }
   }
 
   async inputBonusNumber(winningNumbers) {
-    const _bonusNumber = await this.#readLineAsync(SYSTEM_MESSAGE.INPUT_BONUS_NUMBER);
-    this.validation.validateBonusNumberInputString(_bonusNumber);
-
-    const bonusNumber = Number(_bonusNumber);
-    this.validation.validateBonusNumberRange(bonusNumber, winningNumbers);
-
-    return bonusNumber;
+    try {
+      const _bonusNumber = await this.#readLineAsync(SYSTEM_MESSAGE.INPUT_BONUS_NUMBER);
+      this.validation.validateBonusNumberInputString(_bonusNumber, this.inputBonusNumber);
+      const bonusNumber = Number(_bonusNumber);
+      this.validation.validateBonusNumberRange(bonusNumber, winningNumbers, this.inputBonusNumber);
+      return bonusNumber;
+    } catch (error) {
+      Console.print(error.message);
+      return await this.inputBonusNumber(winningNumbers);
+    }
   }
 
   async #readLineAsync(prompt) {
